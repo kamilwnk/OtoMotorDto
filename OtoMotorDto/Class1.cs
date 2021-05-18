6a863af38d682eff64a05f5d74d180031730eec8
Kamil Wnęk <kamilwnek963@gmail.com>
2021-05-18 18:28:11 +0200

    Created project and added simply models

---
+++ OtoMotorDto/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OtoMotorDto
+{
+    public class Class1
+    {
+    }
+}
---
+++ OtoMotorDto/Products/Motorcycle/MotocycleModelDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtoMotorDto.Products.Motorcycle
+{
+    public class MotocycleModelDto
+    {
+    }
+}
